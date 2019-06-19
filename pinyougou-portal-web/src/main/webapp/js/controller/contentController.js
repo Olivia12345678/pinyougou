dec9c65fe3e55c31b6dd4d0ebe8eb60d63e25274
zhoujh <18260154234@163.com>
2019-06-19 11:08:28 +0800

    redis

---
+++ pinyougou-portal-web/src/main/webapp/js/controller/contentController.js
@@ -0,0 +1,13 @@
+app.controller('contentController',function($scope,contentService){
+	
+	$scope.contentList=[];//广告列表
+	
+	$scope.findByCategoryId=function(categoryId){
+		contentService.findByCategoryId(categoryId).success(
+			function(response){
+				$scope.contentList[categoryId]=response;
+			}
+		);		
+	}
+	
+});
